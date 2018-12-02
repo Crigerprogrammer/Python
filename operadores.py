7ecfd4bdfb7e29abb604e6d519fa994fc265ab33
Crigerprogrammer <crigerprogrammer@gmail.com>
2018-12-02 11:26:36 -0600

    Operadores y Reservadas

---
+++ operadores.py
@@ -0,0 +1,3 @@
+my_var = 2
+my_var = my_var * 5
+print(my_var)
---
+++ reservadas.py
@@ -0,0 +1,10 @@
+"""Asignacion"""
+message = 'Hola'
+print(message)
+"""Privadas"""
+_other_message = 'Hey'
+print(_other_message)
+"""Orden de Operadores"""
+print(2+7/4)
+"""Utilizando  Parentesis"""
+print((2+7)/4)
